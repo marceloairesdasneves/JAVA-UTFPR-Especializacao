ddf7493245399933b040035ff817fa1d2317dece
marcelo aires das neves <mneves230@gmail.com>
2025-09-16 14:52:19 -0300

    Teste netbeans x github Signed-off-by:marcelo aires das neves <mneves230@gmail.com>

---
+++ Documents/NetBeansProjects/OlaMundo/src/main/java/br/com/utfpr/java/olamundo/OlaMundo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.utfpr.java.olamundo;
+
+/**
+ *
+ * @author USER
+ */
+public class OlaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
