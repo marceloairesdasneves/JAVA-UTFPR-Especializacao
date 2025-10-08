c3822747a3fc941282f6d968d2dc75ba00c01706
marceloairesdasneves <141940013+marceloairesdasneves@users.noreply.github.com>
2025-10-08 15:00:19 -0300

    teste Projetoa

---
+++ Documents/NetBeansProjects/projetoa/src/main/java/com/mycompany/projetoa/Projetoa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetoa;
+
+/**
+ *
+ * @author USER
+ */
+public class Projetoa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
